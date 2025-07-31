4d6dda7c1df28c86dac3d92cc4c0474c28d7d35c
Naveen <naveenking2002@gmail.com>
2025-07-31 22:59:29 +0530

    ch

--- js/custom.js
+++ js/custom.js
@@ -71,3 +71,28 @@
 
 
 })()
+
+const swiper = new Swiper(".mySwiper", {
+slidesPerView: 5,
+spaceBetween: 30,
+loop: true,
+autoplay: {
+	delay: 2500,
+	disableOnInteraction: false,
+},
+breakpoints: {
+	320: {
+	slidesPerView: 2,
+	spaceBetween: 20,
+	},
+	768: {
+	slidesPerView: 3,
+	spaceBetween: 25,
+	},
+	992: {
+	slidesPerView: 5,
+	spaceBetween: 30,
+	}
+}
+});
+
